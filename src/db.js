01355fa9bee4678a878a07b00456ae5747be411f
Juan Segundo <juansepincha@gmail.com>
2022-11-30 00:19:40 -0300

    first commit

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
-const { DB_USER,  DB_PASSWORD} = process.env;
+const { DB_USER,  DB_PASSWORD, DB_URL} = process.env;
 const mongoose = require('mongoose');
 
 // mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.b5w0juw.mongodb.net/?retryWrites=true&w=majority
 // mongodb+srv://HenryDB:Henry2022@cluster0.b5w0juw.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect(`mongodb://mongo:YNN1EPa23Wn9VzqQEoEQ@containers-us-west-31.railway.app:6517`)
+mongoose.connect(`${DB_URL}`)
 .then((result) => console.log("Conexion exitosa a la BBDD"))
 .catch((err) => console.log(err));
 
