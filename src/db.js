9dae056a7335b1600fff6398db79e5f5746466f8
Francisco Javier Caballe <francaballe@gmail.com>
2022-11-30 13:54:21 -0300

    dynamic use of env variables according to environment

--- src/db.js
+++ src/db.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const { DB_USER,  DB_PASSWORD, DB_URL} = process.env;
 const mongoose = require('mongoose');
 
-// mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.b5w0juw.mongodb.net/?retryWrites=true&w=majority
-// mongodb+srv://HenryDB:Henry2022@cluster0.b5w0juw.mongodb.net/?retryWrites=true&w=majority
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.b5w0juw.mongodb.net/?retryWrites=true&w=majority`)
 .then((result) => console.log("Conexion exitosa a la BBDD"))
 .catch((err) => console.log(err));
